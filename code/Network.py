5384280f374f631f96b470cd38289e8083d5ffb5
li-2019 <58059830+li-2019@users.noreply.github.com>
2022-03-22 22:06:15 +0800

    Add files via upload

---
+++ code/Network.py
@@ -0,0 +1,283 @@
+from torch import nn
+import torch
+import torchvision
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+
+class USIDNet(nn.Module):
+    def __init__(self):
+        super(USIDNet, self).__init__()
+        self.enc = Encode()
+        self.dec = Decode()
+    def load_weights(self, checkpoint):
+        state_dict = torch.load(checkpoint, map_location='cpu')
+        self.enc.load_state_dict(state_dict['a'], strict=False)
+        self.dec.load_state_dict(state_dict['b'], strict=False)
+    def forward(self,x):
+        return self.dec(self.enc(x))
+
+class Encode(nn.Module):
+    def __init__(self):
+        super(Encode, self).__init__()
+        self.enc = Encoder()
+    def forward(self,x):
+        return self.enc(x)
+class Decode(nn.Module):
+    def __init__(self):
+        super(Decode, self).__init__()
+        self.dec_cont = Decoder()
+    def forward(self,x):
+        return self.dec_cont(x)
+
+class Encoder(nn.Module):
+    def __init__(self, n_downsample=2, input_dim=3, dim=64, norm='in', activ='relu', pad_type='reflect'):
+        super(Encoder, self).__init__()
+        self.model = []
+        self.model += [Conv2dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+        # downsampling blocks
+        for _ in range(n_downsample):
+            self.model += [Conv2dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            dim *= 2
+        for _ in range(4):
+            self.model += [Bottle2neck(in_channels=dim, out_channels=dim, stride=1, stype='normal', norm='in'),]
+        self.model = nn.Sequential(*self.model)
+    def forward(self, x):
+        return self.model(x)
+
+class Decoder(nn.Module):
+    def __init__(self,n_upsample=2, dim=256, activ='relu', pad_type='reflect'):
+        super(Decoder, self).__init__()
+        self.model = []
+        for _ in range(4):
+            self.model += [Bottle2neck(in_channels=dim, out_channels=dim, stride=1, stype='normal', norm='in')]
+        # upsampling blocks
+        for _ in range(n_upsample):
+            self.model += [nn.Upsample(scale_factor=2),
+                           Conv2dBlock(dim, dim // 2, 5, 1, 2, norm='ln', activation=activ, pad_type=pad_type)]
+            dim //= 2
+        self.model += [CALayer(dim), PALayer(dim)]
+        self.model += [Conv2dBlock(dim, 3, 7, 1, 3, norm='none', activation='tanh', pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+    def forward(self, x, x_a=None):
+        output = self.model(x)
+        return output
+
+class PALayer(nn.Module):
+    def __init__(self, channel):
+        super(PALayer, self).__init__()
+        self.pa = nn.Sequential(
+                nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channel // 8, 1, 1, padding=0, bias=True),
+                nn.Sigmoid()
+        )
+    def forward(self, x):
+        y = self.pa(x)
+        return x * y
+
+class CALayer(nn.Module):
+    def __init__(self, channel):
+        super(CALayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.ca = nn.Sequential(
+                nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channel // 8, channel, 1, padding=0, bias=True),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.ca(y)
+        return x * y
+
+class ResBlock(nn.Module):
+    def __init__(self, dim, norm='in', activation='relu', pad_type='zero'):
+        super(ResBlock, self).__init__()
+
+        model = []
+        model += [Conv2dBlock(dim ,dim, 3, 1, 1, norm=norm, activation=activation, pad_type=pad_type)]
+        model += [Conv2dBlock(dim ,dim, 3, 1, 1, norm=norm, activation='none', pad_type=pad_type)]
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        residual = x
+        out = self.model(x)
+        out += residual
+        return out
+
+class Bottle2neck(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None, baseWidth=26, scale=4, stype='normal', norm='bn'):
+        """ Constructor
+        Args:
+            inplanes: input channel dimensionality
+            planes: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            downsample: None when stride = 1
+            baseWidth: basic width of conv3x3
+            scale: number of scale.
+            type: 'normal': normal set. 'stage': first block of a new stage.
+        """
+        super(Bottle2neck, self).__init__()
+        width = out_channels // 4
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm2d
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.bn1 = self.norm(out_channels)
+
+        if scale == 1:
+            self.nums = 1
+        else:
+            self.nums = scale - 1
+        if stype == 'stage':
+            self.pool = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1)
+        convs = []
+        bns = []
+        for i in range(self.nums):
+            convs.append(nn.Conv2d(width, width, kernel_size=3, stride=stride, padding=1, bias=False))
+            bns.append(nn.BatchNorm2d(width))
+        self.convs = nn.ModuleList(convs)
+        self.bns = nn.ModuleList(bns)
+
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
+        self.bn3 = self.norm(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        if stride != 1:
+            downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels,
+                          kernel_size=1, stride=stride, bias=False),
+                self.norm(out_channels),
+            )
+
+        self.downsample = downsample
+        self.stype = stype
+        self.scale = scale
+        self.width = width
+        self.ca = CALayer(in_channels)
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        spx = torch.split(out, self.width, 1)
+        for i in range(self.nums):
+            if i == 0 or self.stype == 'stage':
+                sp = spx[i]
+            else:
+                sp = sp + spx[i]
+            sp = self.convs[i](sp)
+            sp = self.relu(self.bns[i](sp))
+            if i == 0:
+                out = sp
+            else:
+                out = torch.cat((out, sp), 1)
+        if self.scale != 1 and self.stype == 'normal':
+            out = torch.cat((out, spx[self.nums]), 1)
+        elif self.scale != 1 and self.stype == 'stage':
+            out = torch.cat((out, self.pool(spx[self.nums])), 1)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.ca(out)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+class Conv2dBlock(nn.Module):
+    def __init__(self, input_dim , output_dim, kernel_size, stride,
+                 padding=0, norm='none', activation='relu', pad_type='zero'):
+        super(Conv2dBlock, self).__init__()
+        self.use_bias = True
+        # initialize convolution
+        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias)
+        # initialize padding
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+        self.norm_type = norm
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(norm_dim)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm2d(norm_dim)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+    def forward(self, x):
+        x = self.conv(self.pad(x))
+        if self.norm_type != 'wn' and self.norm != None:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+class vgg_19(nn.Module):
+    def __init__(self):
+        super(vgg_19, self).__init__()
+        vgg_model = torchvision.models.vgg19(pretrained=True)
+        self.feature_ext = nn.Sequential(*list(vgg_model.features.children())[:20])
+    def forward(self, x):
+        if x.size(1) == 1:
+            x = torch.cat((x, x, x), 1)
+        out = self.feature_ext(x)
+        return out
+
+class LayerNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        # if x.size(0) == 1:
+        #     # These two lines run much faster in pytorch 0.4 than the two lines listed below.
+        #     mean = x.view(-1).mean().view(*shape)
+        #     std = x.view(-1).std().view(*shape)
+        # else:
+        mean = x.view(x.size(0), -1).mean(1).view(*shape)
+        std = x.view(x.size(0), -1).std(1).view(*shape)
+
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
---
+++ code/evaluate.py
@@ -0,0 +1,152 @@
+from code.Network import USIDNet
+import argparse
+import torchvision.utils as vutils
+import torch
+import os
+from torchvision import transforms
+from PIL import Image
+from skimage.measure import compare_psnr, compare_ssim
+import numpy as np
+import math
+irange = range
+
+
+def load_image(in_dir, gt_dir):
+    in_path = [os.path.join(in_dir, x) for x in sorted(os.listdir(in_dir))]
+    gt_path = [os.path.join(gt_dir, x) for x in sorted(os.listdir(gt_dir))]
+    return zip(in_path, gt_path)
+def save_image(tensor, filename, nrow=8, padding=2,
+               normalize=False, range=None, scale_each=False, pad_value=0):
+    from PIL import Image
+    grid = make_grid(tensor, nrow=nrow, padding=padding, pad_value=pad_value,
+                     normalize=normalize, range=range, scale_each=scale_each)
+    ndarr = grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+    im = Image.fromarray(ndarr)
+    im.save(filename)
+def make_grid(tensor, nrow=8, padding=2,
+              normalize=False, range=None, scale_each=False, pad_value=0):
+    """Make a grid of images.
+
+    Args:
+        tensor (Tensor or list): 4D mini-batch Tensor of shape (B x C x H x W)
+            or a list of images all of the same size.
+        nrow (int, optional): Number of images displayed in each row of the grid.
+            The final grid size is ``(B / nrow, nrow)``. Default: ``8``.
+        padding (int, optional): amount of padding. Default: ``2``.
+        normalize (bool, optional): If True, shift the image to the range (0, 1),
+            by the min and max values specified by :attr:`range`. Default: ``False``.
+        range (tuple, optional): tuple (min, max) where min and max are numbers,
+            then these numbers are used to normalize the image. By default, min and max
+            are computed from the tensor.
+        scale_each (bool, optional): If ``True``, scale each image in the batch of
+            images separately rather than the (min, max) over all images. Default: ``False``.
+        pad_value (float, optional): Value for the padded pixels. Default: ``0``.
+
+    Example:
+        See this notebook `here <https://gist.github.com/anonymous/bf16430f7750c023141c562f3e9f2a91>`_
+
+    """
+    if not (torch.is_tensor(tensor) or
+            (isinstance(tensor, list) and all(torch.is_tensor(t) for t in tensor))):
+        raise TypeError('tensor or list of tensors expected, got {}'.format(type(tensor)))
+
+    # if list of tensors, convert to a 4D mini-batch Tensor
+    if isinstance(tensor, list):
+        tensor = torch.stack(tensor, dim=0)
+
+    if tensor.dim() == 2:  # single image H x W
+        tensor = tensor.unsqueeze(0)
+    if tensor.dim() == 3:  # single image
+        if tensor.size(0) == 1:  # if single-channel, convert to 3-channel
+            tensor = torch.cat((tensor, tensor, tensor), 0)
+        tensor = tensor.unsqueeze(0)
+
+    if tensor.dim() == 4 and tensor.size(1) == 1:  # single-channel images
+        tensor = torch.cat((tensor, tensor, tensor), 1)
+
+    if normalize is True:
+        tensor = tensor.clone()  # avoid modifying tensor in-place
+        if range is not None:
+            assert isinstance(range, tuple), \
+                "range has to be a tuple (min, max) if specified. min and max are numbers"
+
+        def norm_ip(img, min, max):
+            img.clamp_(min=min, max=max)
+            img.add_(-min).div_(max - min + 1e-5)
+
+        def norm_range(t, range):
+            if range is not None:
+                norm_ip(t, range[0], range[1])
+            else:
+                norm_ip(t, float(t.min()), float(t.max()))
+
+        if scale_each is True:
+            for t in tensor:  # loop over mini-batch dimension
+                norm_range(t, range)
+        else:
+            norm_range(tensor, range)
+
+    if tensor.size(0) == 1:
+        return tensor.squeeze(0)
+
+    # make the mini-batch of images into a grid
+    nmaps = tensor.size(0)
+    xmaps = min(nrow, nmaps)
+    ymaps = int(math.ceil(float(nmaps) / xmaps))
+    height, width = int(tensor.size(2) + padding), int(tensor.size(3) + padding)
+    grid = tensor.new_full((3, height * ymaps + padding, width * xmaps + padding), pad_value)
+    k = 0
+    for y in irange(ymaps):
+        for x in irange(xmaps):
+            if k >= nmaps:
+                break
+            grid.narrow(1, y * height + padding, height - padding)\
+                .narrow(2, x * width + padding, width - padding)\
+                .copy_(tensor[k])
+            k = k + 1
+    return grid
+
+def test(model):
+    transform = transforms.Compose([transforms.ToTensor()])
+    psnr = 0
+    ssim = 0
+    count=0
+    with torch.no_grad():
+        for in_img_path, gt_img_path in imgs:
+            # Load images
+            in_img = transform(Image.open(in_img_path).convert('RGB')).unsqueeze(0).cuda()
+            gt_img = Image.open(gt_img_path).convert('RGB')
+            gt_img = np.array(gt_img) / 255.
+            outputs = model(in_img)
+            # Save dehazed images
+            path = os.path.join(opts.output_dir, str(count+1) + '.png')
+            out = outputs.clone()
+            vutils.save_image(outputs.data, path)
+            # Calcualte PSNR and SSIM
+            out = torch.squeeze(out)
+            out = out.clamp_(0, 1).permute(1, 2, 0).to('cpu', torch.float64).numpy()
+            psnr += compare_psnr(gt_img, out)
+            ssim += compare_ssim(gt_img, out, multichannel=True)
+            count+=1
+        print('The number of images:{}'.format(count))
+    psnr /= count
+    ssim /= count
+    print('PSNR:{}, SSIM:{}'.format(psnr, ssim))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_dir', type=str, default='../data/HSTS/hazy/', help="hazy images")
+    parser.add_argument('--gt_dir', type=str, default='../data/HSTS/gt/', help="target images")
+    parser.add_argument('--output_dir', type=str, default='../results/', help="output images")
+    parser.add_argument('--checkpoint_path', type=str, default='../checkpoints/model.pt')
+    opts = parser.parse_args()
+    if not os.path.exists(opts.output_dir):
+        os.makedirs(opts.output_dir)
+    torch.manual_seed(10)
+    torch.cuda.manual_seed(10)
+    model = USIDNet()
+    model.cuda()
+    model.eval()
+    model.load_weights(opts.checkpoint_path)
+    imgs = load_image(opts.in_dir, opts.gt_dir)
+    test(model)
