aae9e93467c22744d058716393e907cd7f8139d2
CVsipl <100458096+BJUTsipl@users.noreply.github.com>
2023-07-17 11:25:42 +0800

    Add files via upload

---
+++ code/utils.py
@@ -0,0 +1,256 @@
+from torch.utils.data import DataLoader
+from networks import vgg_19
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+from torchvision import transforms
+from data import ImageFilelist, ImageFolder
+import torch
+import os
+import math
+import torchvision.utils as vutils
+import yaml
+import numpy as np
+import torch.nn.init as init
+import time
+
+
+def get_all_data_loaders(conf):
+    batch_size = conf['batch_size']
+    num_workers = conf['num_workers']
+    height = conf['crop_image_height']
+    width = conf['crop_image_width']
+    data_type = conf['data_type']
+
+    train_loader_a = get_data_loader_folder(conf['train_H'], batch_size, train=True,  height=height, width=width,  crop=True, data_name='trainA', data_type= data_type)
+    train_loader_b = get_data_loader_folder(conf['train_G'], batch_size, train=True,  height=height, width=width,  crop=True, data_name='trainB', data_type= data_type)
+    val_loader_a = get_data_loader_folder(conf['val_H'], 1, train=False,  height=height, width=width, crop=False, data_name='valA', data_type= data_type)
+    val_loader_b = get_data_loader_folder(conf['val_G'], 1, train=False,  height=height, width=width, crop=False, data_name='valB', data_type= data_type)
+    test_loader_real = get_data_loader_folder(conf['test_H'], 1, train=False,  height=height, width=width,  crop=False, data_name='testA', data_type= data_type)
+
+    return train_loader_a, train_loader_b,  val_loader_a, val_loader_b, test_loader_real
+
+
+def get_data_loader_folder(input_folder, batch_size, train, new_size=None,
+                           height=128, width=256, num_workers=4, crop=True, data_name=None, data_type=None):
+    transform_list = [transforms.ToTensor()] # 
+    transform_list = [transforms.RandomResizedCrop(128, scale=(0.5, 0.7))] + transform_list if crop else transform_list  # RandomCrop
+    transform_list = [transforms.RandomHorizontalFlip()] + transform_list if train else transform_list
+    transform = transforms.Compose(transform_list)
+    dataset = ImageFolder(input_folder, transform=transform, data_name=data_name, data_type=data_type)
+    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=train, drop_last=True, num_workers=num_workers)
+    return loader
+
+class data_prefetcher():
+    def __init__(self, loader):
+        self.loader = iter(loader)
+        self.stream = torch.cuda.Stream()
+        self.preload()
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        input = self.next_input
+        self.preload()
+        return input
+    def preload(self):
+        try:
+            self.next_input = next(self.loader)
+        except StopIteration:
+            self.next_input = None
+            return 
+        with torch.cuda.stream(self.stream):
+            self.next_input = self.next_input.cuda(non_blocking=True)
+
+def get_config(config):
+    with open(config, 'r') as stream:
+        return yaml.load(stream)
+
+
+def eformat(f, prec):
+    s = "%.*e"%(prec, f)
+    mantissa, exp = s.split('e')
+    # add 1 to digits as 1 is taken by sign +/-
+    return "%se%d"%(mantissa, int(exp))
+
+
+def __write_images(image_outputs, display_image_num, file_name):
+    image_tensor = torch.cat([images[:display_image_num] for images in image_outputs], 0)
+    image_grid = vutils.make_grid(image_tensor.data, nrow=display_image_num, padding=0, normalize=False)
+    vutils.save_image(image_grid, file_name, nrow=1)
+
+
+def write_2images(image_outputs, display_image_num, image_directory, postfix):
+    n = len(image_outputs)
+    __write_images(image_outputs, display_image_num, '%s/gen_%s.jpg' % (image_directory, postfix))
+    # __write_images(image_outputs[n//2:n], display_image_num, '%s/gen_b2a_%s.jpg' % (image_directory, postfix))
+
+
+def prepare_sub_folder(output_directory):
+    image_directory = os.path.join(output_directory, 'images')
+    if not os.path.exists(image_directory):
+        print("Creating directory: {}".format(image_directory))
+        os.makedirs(image_directory)
+    checkpoint_directory = os.path.join(output_directory, 'checkpoints')
+    if not os.path.exists(checkpoint_directory):
+        print("Creating directory: {}".format(checkpoint_directory))
+        os.makedirs(checkpoint_directory)
+    return checkpoint_directory, image_directory
+
+
+def write_one_row_html(html_file, iterations, img_filename, all_size):
+    html_file.write("<h3>iteration [%d] (%s)</h3>" % (iterations,img_filename.split('/')[-1]))
+    html_file.write("""
+        <p><a href="%s">
+          <img src="%s" style="width:%dpx">
+        </a><br>
+        <p>
+        """ % (img_filename, img_filename, all_size))
+    return
+
+
+def write_html(filename, iterations, image_save_iterations, image_directory, all_size=1536):
+    html_file = open(filename, "w")
+    html_file.write('''
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Experiment name = %s</title>
+      <meta http-equiv="refresh" content="30">
+    </head>
+    <body>
+    ''' % os.path.basename(filename))
+    html_file.write("<h3>current</h3>")
+    write_one_row_html(html_file, iterations, '%s/gen_a2b_train_current.jpg' % (image_directory), all_size)
+    write_one_row_html(html_file, iterations, '%s/gen_b2a_train_current.jpg' % (image_directory), all_size)
+    for j in range(iterations, image_save_iterations-1, -1):
+        if j % image_save_iterations == 0:
+            write_one_row_html(html_file, j, '%s/gen_a2b_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_b2a_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_a2b_train_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_b2a_train_%08d.jpg' % (image_directory, j), all_size)
+    html_file.write("</body></html>")
+    html_file.close()
+
+
+def write_loss(iterations, trainer, train_writer):
+    members = [attr for attr in dir(trainer) \
+               if not callable(getattr(trainer, attr)) and not attr.startswith("__") and ('loss' in attr or 'grad' in attr or 'nwd' in attr)]
+    for m in members:
+        train_writer.add_scalar(m, getattr(trainer, m), iterations + 1)
+
+
+def slerp(val, low, high):
+    """
+    original: Animating Rotation with Quaternion Curves, Ken Shoemake
+    https://arxiv.org/abs/1609.04468
+    Code: https://github.com/soumith/dcgan.torch/issues/14, Tom White
+    """
+    omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
+    so = np.sin(omega)
+    return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high
+
+
+def get_slerp_interp(nb_latents, nb_interp, z_dim):
+    """
+    modified from: PyTorch inference for "Progressive Growing of GANs" with CelebA snapshot
+    https://github.com/ptrblck/prog_gans_pytorch_inference
+    """
+
+    latent_interps = np.empty(shape=(0, z_dim), dtype=np.float32)
+    for _ in range(nb_latents):
+        low = np.random.randn(z_dim)
+        high = np.random.randn(z_dim)  # low + np.random.randn(512) * 0.7
+        interp_vals = np.linspace(0, 1, num=nb_interp)
+        latent_interp = np.array([slerp(v, low, high) for v in interp_vals],
+                                 dtype=np.float32)
+        latent_interps = np.vstack((latent_interps, latent_interp))
+
+    return latent_interps[:, :, np.newaxis, np.newaxis]
+
+
+# Get model list for resume
+def get_model_list(dirname, key):
+    if os.path.exists(dirname) is False:
+        return None
+    gen_models = [os.path.join(dirname, f) for f in os.listdir(dirname) if
+                  os.path.isfile(os.path.join(dirname, f)) and key in f and ".pt" in f]
+    if gen_models is None:
+        return None
+    gen_models.sort()
+    last_model_name = gen_models[-1]
+    return last_model_name
+
+
+def load_vgg19():
+    """ Use the model from https://github.com/abhiskk/fast-neural-style/blob/master/neural_style/utils.py """
+    # if not os.path.exists(model_dir):
+    #     os.mkdir(model_dir)
+    # if not os.path.exists(os.path.join(model_dir, 'vgg16.weight')):
+    #     if not os.path.exists(os.path.join(model_dir, 'vgg16.t7')):
+    #         os.system('wget https://www.dropbox.com/s/76l3rt4kyi3s8x7/vgg16.t7?dl=1 -O ' + os.path.join(model_dir, 'vgg16.t7'))
+    #     vgglua = load_lua(os.path.join(model_dir, 'vgg16.t7'))
+    #     vgg = Vgg16()
+    #     for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
+    #         dst.data[:] = src
+    #     torch.save(vgg.state_dict(), os.path.join(model_dir, 'vgg16.weight'))
+    # vgg = Vgg16()
+    # vgg.load_state_dict(torch.load(os.path.join(model_dir, 'vgg16.weight')))
+    vgg = vgg_19()
+    return vgg
+
+
+def vgg_preprocess(batch):
+    tensortype = type(batch.data)
+    (r, g, b) = torch.chunk(batch, 3, dim = 1)
+    batch = torch.cat((b, g, r), dim = 1) # convert RGB to BGR
+    batch = batch * 255       #   * 0.5  [-1, 1] -> [0, 255]
+    mean = tensortype(batch.data.size()).cuda()
+    mean[:, 0, :, :] = 103.939
+    mean[:, 1, :, :] = 116.779
+    mean[:, 2, :, :] = 123.680
+    batch = batch.sub(Variable(mean)) # subtract mean
+    return batch
+
+
+def get_scheduler(optimizer, hyperparameters, iterations=-1):
+    if 'lr_policy' not in hyperparameters or hyperparameters['lr_policy'] == 'constant':
+        scheduler = None # constant scheduler
+    elif hyperparameters['lr_policy'] == 'step':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=hyperparameters['step_size'],
+                                        gamma=hyperparameters['gamma'], last_epoch=iterations)
+    else:
+        return NotImplementedError('learning rate policy [%s] is not implemented', hyperparameters['lr_policy'])
+    return scheduler
+
+
+def weights_init(init_type='gaussian'):
+    def init_fun(m):
+        classname = m.__class__.__name__
+        if (classname.find('Conv') == 0 or classname.find('Linear') == 0) and hasattr(m, 'weight'):
+            # print m.__class__.__name__
+            if init_type == 'gaussian':
+                init.normal_(m.weight.data, 0.0, 0.02)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'default':
+                pass
+            else:
+                assert 0, "Unsupported initialization: {}".format(init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+
+    return init_fun
+
+
+class Timer:
+    def __init__(self, msg):
+        self.msg = msg
+        self.start_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        print(self.msg % (time.time() - self.start_time))
